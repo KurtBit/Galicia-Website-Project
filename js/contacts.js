2b98f9cf4a0177c6783fd948e15e32c5b806e46e
Ivory Tailor <ufo.terziev@gmail.com>
2017-03-16 09:49:21 +0200

    Added jQuery validation

--- js/contacts.js
+++ js/contacts.js
@@ -29,4 +29,42 @@ $(document).ready(function () {
 
         return false;
     });
+
+    //Form Validation
+    $("#contact-form").validate({
+        rules: {
+            "contacts-name": {
+                required: true,
+                rangelenght: [3, 10]
+            },
+            "contacts-email": {
+                required: true,
+                email: true
+            },
+            "contacts-phone": {
+                required: true
+            },
+            "contacts-form-content": {
+                required: true,
+                rangelenght: [10, 200]          
+            }
+        },
+        messages: {
+            "contacts-name": {
+                required: "Your user name is required!",
+                rangelenght: "Your name must be between {0} and {1} characters long!"
+            },
+            "contacts-email": {
+                required: "Your email is required!",
+                email: "You will have to enter a valid email!"
+            },
+            "contacts-phone": {
+                required: "Your telephone number is required!"
+            },
+            "contacts-form-content": {
+                required: "Please state how can we help you?",
+                rangelenght: "Please enter text between {0} and {1} charaters long."          
+            }
+        }
+    })
 })
