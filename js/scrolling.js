cf1c4c9156002b2a35755ad565f328e712d2224c
Dominent <petromilpavlov@gmail.com>
2017-03-22 21:47:36 +0200

    Fixed navigation.

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,26 +1,28 @@
 $(document).ready(function () {
+    const PADDING = 50;
+
     $('#bs-example-navbar-collapse-1 > ul > li:nth-child(1) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('.js-home').offset().top}, 1000);
+        $('html body').animate({scrollTop: $('.js-home').offset().top - PADDING}, 1000);
     })
 
     $('#bs-example-navbar-collapse-1 > ul > li:nth-child(2) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('.js-about').offset().top}, 1000);
+        $('html body').animate({scrollTop: $('.js-about').offset().top - PADDING}, 1000) ;
     })
 
      $('#bs-example-navbar-collapse-1 > ul > li:nth-child(3) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('.js-gallery').offset().top}, 1000);
+        $('html body').animate({scrollTop: $('.js-gallery').offset().top - PADDING}, 1000);
     })
 
     $('#bs-example-navbar-collapse-1 > ul > li:nth-child(4) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('.js-services').offset().top}, 1000);
+        $('html body').animate({scrollTop: $('.js-services').offset().top - PADDING}, 1000);
     })
 
     $('#bs-example-navbar-collapse-1 > ul > li:nth-child(5) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('.js-collaborators').offset().top}, 1000);
+        $('html body').animate({scrollTop: $('.js-collaborators').offset().top - PADDING}, 1000);
     })
 
     $('#bs-example-navbar-collapse-1 > ul > li:nth-child(6) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('.js-contacts').offset().top}, 1000);
+        $('html body').animate({scrollTop: $('.js-contacts').offset().top - PADDING}, 1000);
     })
 
     // js-about
