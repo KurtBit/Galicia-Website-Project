bc4f1dd8d3be215848940633665155f47669e760
Dominent <petromilpavlov@gmail.com>
2017-06-29 19:05:05 +0300

    Working on mongo db integration

--- calendar.js
+++ calendar.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const _ = require('lodash');
+const mongo = require('mongodb');
 
 var router = express.Router();
+
+// TODO(Domi): Create config file and store user and password in there!
+const user = 'website';
+const password = 'shamS6rmWM';
+const db_appointments_table_name = 'appointments';
+
+const url = `mongodb://${user}:${password}@ds064649.mlab.com:64649/galiciq_cms_db`
 
 function getMaxDaysInMonth(month, year) {
     return (new Date(month, year, 0).getDate());
@@ -10,6 +18,27 @@ function getMaxDaysInMonth(month, year) {
 router.get('/', function (req, res) {
     let date = new Date();
     let maxDaysInMonth = getMaxDaysInMonth(date.getMonth(), date.getMonth());
+
+    let appointment = {
+        name: 'Ivan',
+        date: '01.02.2016',
+        comment: 'Бърза Прическа',
+        time: '13:00'
+    }
+
+    mongo.connect(url, function(err, db){
+        if(!db){
+            console.log(err);
+            return;
+        }
+        db.collection(db_appointments_table_name).insertOne(appointment, function(err, result){
+            if(err){
+               console.log(err);
+            }
+
+            db.close();
+        })
+    })
 
     let calendarCells = _.range(1, maxDaysInMonth, 1);
 
