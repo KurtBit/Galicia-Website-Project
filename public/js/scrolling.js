34163a0d0031b73b2146863e1a0b8a79fe233593
Ivory Tailor <ufo.terziev@gmail.com>
2017-07-15 14:00:50 +0300

    Fix scrolling issue

--- public/js/scrolling.js
+++ public/js/scrolling.js
@@ -1,34 +1,46 @@
 $(document).ready(function () {
     const PADDING = 50;
 
-    $('#bs-example-navbar-collapse-1 > ul > li:nth-child(1) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('#js-jumbotron').offset().top - PADDING}, 1000);
+    $('#js-scroll-to-about').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('#js-about').offset().top - PADDING
+        }, 1000);
     })
 
-    $('#bs-example-navbar-collapse-1 > ul > li:nth-child(2) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('#js-about').offset().top - PADDING}, 1000) ;
+    $('#js-scroll-to-gallery').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('#js-gallery').offset().top - PADDING
+        }, 1000);
     })
 
-     $('#bs-example-navbar-collapse-1 > ul > li:nth-child(3) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('#js-gallery').offset().top - PADDING}, 1000);
+    $('#js-scroll-to-services').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('#js-services').offset().top - PADDING
+        }, 1000);
     })
 
-    $('#bs-example-navbar-collapse-1 > ul > li:nth-child(4) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('#js-services').offset().top - PADDING}, 1000);
+    $('#js-scroll-to-courses').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('#js-courses').offset().top - PADDING
+        }, 1000);
     })
 
-    $('#bs-example-navbar-collapse-1 > ul > li:nth-child(5) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('#js-contacts').offset().top - PADDING}, 1000);
+    $('#js-scroll-to-contacts').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('#js-contacts').offset().top - PADDING
+        }, 1000);
     })
 
-    $('#bs-example-navbar-collapse-1 > ul > li:nth-child(6) > a').on('click', function() {
-        $('html body').animate({scrollTop: $('#js-map').offset().top - PADDING}, 1000);
+    $('#js-scroll-to-map').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $('#js-map').offset().top - PADDING
+        }, 1000);
     })
 
     // js-about
     // js-gallery
     // js-services
-    // js-collaborators
+    // js-courses
     // js-contacts
     // js-map
 });
